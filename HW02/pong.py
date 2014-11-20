6db4755c81e653de1c6a51c5cc186e3e8274fa1c
hadrianpaulo <hadrianpaulo@gmail.com>
2014-11-21 01:23:02 +0800

    Added Wall Class and Implemented Collision Detection for Paddle Class

--- HW02/pong.py
+++ HW02/pong.py
@@ -14,11 +14,37 @@ class Paddle(pygame.sprite.Sprite):
 		# Specify initial coordinates
 		self.rect.x = x
 		self.rect.y = y
+		# Specify reset position
+		self.xinit = x
+		self.yinit = y
 	def moveUp(self):
 		self.rect.y -= 10
 	#def moveDown JAC LIN
 
+	def checkCollision(self, sprite2):
+		result = pygame.sprite.collide_rect(self, sprite2)
+		if result == True:
+			self.resetPosition()
+
+	def resetPosition(self):
+		self.rect.x = self.xinit
+		self.rect.y = self.yinit
+
 # Ball class with __init__ JAC LIN
+
+# Wall Class
+class Wall(pygame.sprite.Sprite):
+	def __init__(self,width,height,x,y):
+		pygame.sprite.Sprite.__init__(self) 
+		# Specify surface with dimensions (width x height)
+		self.image = pygame.Surface((width,height))
+		# Load image and set background to alpha
+		pygame.draw.rect(self.image,(255,255,255),[0,0,width,height])
+		self.rect = self.image.get_rect()
+		# Specify initial coordinates
+		self.rect.x = x
+		self.rect.y = y
+
 
 # initialize pygame
 pygame.init()
@@ -34,13 +60,20 @@ clock=pygame.time.Clock()
 # This is the sprite list. All you need to know is that this list is used for rendering/updating ALL the graphics/images it has :)
 spritesList = pygame.sprite.RenderPlain()
 
+#Declare Walls
+topWall = Wall(400,2,0,0)
+bottomWall = Wall(400,2,0,398)
+leftWall = Wall(2,400,0,0)
+rightWall = Wall(2,400,398,0)
+
 #Declare player 1 Paddle with starting X,Y coordinates
-player1 = Paddle(50,200)
+player1 = Paddle(20,150)
 #declare a player 2 JAC LIN :)
 
 # Add player 1 to spritesList
 # Every graphical object should be added to spritesList
-spritesList.add(player1)
+spritesList.add(player1,topWall,bottomWall,leftWall,rightWall)
+
 
 while 1:
     for event in pygame.event.get(): # User did something
@@ -58,6 +91,7 @@ while 1:
     # Draws or "Blits" all the graphics in spritesList
     spritesList.draw(screen)
 
+    player1.checkCollision(topWall)
     #FPS
     clock.tick(40)
     
